8b1df093aa3d0cfbcb00a3491d16859bc293887c
Abhinay <abhinayrana46@gmail.com>
2026-08-07 15:09:22 +0530

    Did some basic Read-Eval-Print Loop, exit and echo builtin and implemented type.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String args[]) throws Exception
     {
+        Scanner scanner = new Scanner(System.in);
+        while(true)
+        {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            if(input.startsWith("type ")) {
+                String target = input.substring(5).trim();
+                if (isBuiltIn(target)) {
+                    System.out.println(input + " is a shell builtin");
+                }
+                else
+                {
+                    System.out.println(input + ": not found");
+                }
+            }
+            else if(input.equals("exit"))
+            {
+                break;
+            }
+            else if(input.startsWith("echo "))
+            {
+                System.out.println(input.substring(5).trim());
+            }
+            else
+            System.out.println(input + ": command not found");
+        }
+    }
 
+    private static boolean isBuiltIn(String target)
+    {
+        return target.equals("echo") || target.equals("exit") || target.equals("type");
     }
 }
